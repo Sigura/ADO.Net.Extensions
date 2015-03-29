5f2a6370d03578669aaa04cc2b995777df98f7b3
Artur Dudnik <adudnik@gmail.com>
2015-03-29 16:40:26 +0100

    reorganized solution, samples added

--- README.md
+++ README.md
@@ -14,46 +14,90 @@ MIT licence
 
 ### create connection
 
+```c#
     using(var con = new SqlConnection(connString).OpenIt())
     {
+        Console.WriteLine(@"All is ok {0}", con.State);
     }
+```
 
 ### create command
 
+```c#
     using(var con = new SqlConnection(connString).OpenIt())
     using(var cmd = con.CreateCommand(@"select top 1 getutcdate()"))
     {
+        var utcDateTime = cmd.ExecuteScalar<DateTime>();
+
+        Console.WriteLine(@"Scalar {0}", utcDateTime);
     }
+```
 
 ### create command to execute sproc
 
+```c#
+    using(var con = new SqlConnection(connString).OpenIt())
+    using(var cmd = con.CreateSProcExecCommand(@"[sp_who2]"))
+    using(var res = cmd.ExecuteReader())
+    {
+        while (res.Read())
+        {
+            Console.WriteLine(@"SPID: {0}, Login: {1}", res.GetString(@"SPID").Trim(), res.GetString(@"Login"));
+        }
+    }
+```
+
+### create command to execute sproc with simple mapping
+
+```c#
     using(var con = new SqlConnection(connString).OpenIt())
     using(var cmd = con.CreateSProcExecCommand(@"[sp_who2]"))
     {
+        var rows = cmd.Execute(x => new DataRow
+            {
+                ID = Convert.ToInt32(x.GetString(@"SPID").Trim()),
+                Description = x.GetString(@"Login").Trim()
+            })
+            .Reverse()
+            .Take(10);
+        foreach (var row in rows)
+        {
+            Console.WriteLine(@"SPID: {0}, Login: {1}", row.ID, row.Description);
+        }
     }
 
 ### add param to command
 
+```c#
     using(var con = new SqlConnection(connString).OpenIt())
     using(var cmd = con.CreateCommand(@"select top 1 @param")
                        .AddParam(@"param", 1))
     {
+        var param = cmd.ExecuteScalar<int>();
+
+        Console.WriteLine(@"Param added {0}", param);
     }
+```
 
 ### add out param to command
 
+```c#
     using(var con = new SqlConnection(connString).OpenIt())
     using(var cmd = con.CreateCommand(@"set @param = 10;")
                        .AddOutParam(@"param", 1))
     {
-        command.Execute();
+        cmd.Execute();
 
-        var param = command.Parameters[@"param"] as SqlParameter;
+        var param = cmd.Parameters["@param"] as SqlParameter;
 
+        Console.WriteLine(@"out param {0}", param.Value);
     }
+```
 
-### add data table as param
+### add data table as param with mapping for simple result
+do not use that map way in production
 
+```c#
     using(var con = new SqlConnection(connString).OpenIt())
     using(var cmd = con.CreateCommand(@"select ID from simpleTableWithOneColumnLikeID")
                        .AddParam(
@@ -63,21 +107,29 @@ MIT licence
                         )
          )
     {
-        command.Execute();
+        var rows = cmd.Execute<DataRow>();
 
-        var param = command.Parameters[@"param"] as SqlParameter;
+        foreach (var row in rows)
+        {
+            Console.WriteLine(@"id: {0}", row.ID);
+        }
     }
+```
 
 ### use approle context
 
+```c#
     using(var con = new SqlConnection(connString).OpenIt())
     using(con.SetAppRole(@"appRole", superSicretSrting))
     {
     }
+```
 
 ### set deadlock priority
 
+```c#
     using(var con = new SqlConnection(connString).OpenIt())
     using(con.SetDeadlockPriority(9))
     {
     }
+```
---
+++ Samples/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security;
+
+namespace Samples
+{
+    class Program
+    {
+        private const string ConnString = @"Data Source=.;Initial Catalog=test-db;Integrated Security=True;";
+
+        /// <summary>
+        /// CREATE DATABASE [test-db];
+        /// CREATE TYPE dbo.SimpleTableWithOneColumnLikeID AS TABLE 
+        /// (
+        /// 	ID int not null
+        ///     PRIMARY KEY(ID)
+        /// )
+        /// </summary>
+        static void Main()
+        {
+            using (var con = new SqlConnection(ConnString).OpenIt())
+            {
+                Console.WriteLine(@"All is ok {0}", con.State);
+            }
+
+            using (var con = new SqlConnection(ConnString).OpenIt())
+            using (var cmd = con.CreateCommand(@"select top 1 getutcdate()"))
+            {
+                var utcDateTime = cmd.ExecuteScalar<DateTime>();
+
+                Console.WriteLine(@"Scalar {0}", utcDateTime);
+            }
+
+            using (var con = new SqlConnection(ConnString).OpenIt())
+            using (var cmd = con.CreateSProcExecCommand(@"[sp_who2]"))
+            using (var res = cmd.ExecuteReader())
+            {
+                while (res.Read())
+                {
+                    Console.WriteLine(@"SPID: {0}, Login: {1}", res.GetString(@"SPID").Trim(), res.GetString(@"Login"));
+                }
+            }
+
+            using (var con = new SqlConnection(ConnString).OpenIt())
+            using (var cmd = con.CreateSProcExecCommand(@"[sp_who2]"))
+            {
+                var rows = cmd.Execute(x => new DataRow
+                    {
+                        ID = Convert.ToInt32(x.GetString(@"SPID").Trim()),
+                        Description = x.GetString(@"Login").Trim()
+                    })
+                    .Reverse()
+                    .Take(10);
+                foreach (var row in rows)
+                {
+                    Console.WriteLine(@"SPID: {0}, Login: {1}", row.ID, row.Description);
+                }
+            }
+
+            using (var con = new SqlConnection(ConnString).OpenIt())
+            using (var cmd = con.CreateCommand(@"select top 1 @param")
+                               .AddParam(@"param", 1))
+            {
+                var param = cmd.ExecuteScalar<int>();
+
+                Console.WriteLine(@"Param added {0}", param);
+            }
+
+            using (var con = new SqlConnection(ConnString).OpenIt())
+            using (var cmd = con.CreateCommand(@"set @param = 10;")
+                               .AddOutParam("param", 1))
+            {
+                cmd.Execute();
+
+                var param = cmd.Parameters["@param"] as SqlParameter;
+
+                Console.WriteLine(@"out param {0}", param.Value);
+            }
+
+            using (var con = new SqlConnection(ConnString).OpenIt())
+            using (var cmd = con.CreateCommand(@"select id from @simpleTableWithOneColumnLikeID")
+                               .AddParam(
+                                    @"simpleTableWithOneColumnLikeID",
+                                    new[] { 1, 2, 3, 4 }.ToDataTable(@"id"),
+                                    @"dbo.SimpleTableWithOneColumnLikeID"
+                                )
+                 )
+            {
+                var rows = cmd.Execute<DataRow>();
+
+                foreach (var row in rows)
+                {
+                    Console.WriteLine(@"id: {0}", row.ID);
+                }
+            }
+
+            using (var con = new SqlConnection(ConnString).OpenIt())
+            using (con.SetDeadlockPriority(9))
+            {
+            }
+
+            var superSicretSrting = new SecureString();
+
+            /// CREATE APPLICATION ROLE appRole WITH PASSWORD = ''
+            using (var con = new SqlConnection(ConnString).OpenIt())
+            using (con.SetAppRole(@"appRole", superSicretSrting))
+            {
+                Console.WriteLine(@"All is ok {0}", con.State);
+            }
+
+        }
+
+
+        public class DataRow
+        {
+            public int ID;
+            public string Description;
+        }
+    }
+}
